b769e0e718d97ece41f2f6d480a9249fd36df06d
Gourav Sapaliga <130655508+shadowofdominance@users.noreply.github.com>
2026-03-02 11:43:25 +0530

    Initial commit

---
+++ build/Desktop_Qt_6_10_2_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
@@ -0,0 +1,3 @@
+set(__QT_DEPLOY_TARGET_TutorialLearning_FILE C:/Users/acd07/Documents/QT_Projects/TutorialLearning/build/Desktop_Qt_6_10_2_MinGW_64_bit-Debug/TutorialLearning.exe)
+set(__QT_DEPLOY_TARGET_TutorialLearning_TYPE EXECUTABLE)
+set(__QT_DEPLOY_TARGET_TutorialLearning_RUNTIME_DLLS C:/Qt/6.10.2/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.10.2/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.10.2/mingw_64/bin/Qt6Core.dll)
---
+++ build/Desktop_Qt_6_10_2_MinGW_64_bit-Debug/.qt/deploy_TutorialLearning_644bf891a8.cmake
@@ -0,0 +1,8 @@
+include("C:/Users/acd07/Documents/QT_Projects/TutorialLearning/build/Desktop_Qt_6_10_2_MinGW_64_bit-Debug/.qt/QtDeploySupport.cmake")
+include("${CMAKE_CURRENT_LIST_DIR}/TutorialLearning-plugins.cmake" OPTIONAL)
+set(__QT_DEPLOY_I18N_CATALOGS "qtbase")
+
+qt6_deploy_runtime_dependencies(
+    EXECUTABLE "C:/Users/acd07/Documents/QT_Projects/TutorialLearning/build/Desktop_Qt_6_10_2_MinGW_64_bit-Debug/TutorialLearning.exe"
+    GENERATE_QT_CONF
+)
